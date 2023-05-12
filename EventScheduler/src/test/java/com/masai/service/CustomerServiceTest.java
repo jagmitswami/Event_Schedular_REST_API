70971bf3365078f57a86d7a3cfb49576f7ce590b
Jagmit <bitscotch@gmail.com>
2023-05-12 23:48:40 +0530

    Added tests for Customer Service

--- EventScheduler/src/test/java/com/masai/service/CustomerServiceTest.java
+++ EventScheduler/src/test/java/com/masai/service/CustomerServiceTest.java
@@ -1,8 +1,114 @@
 package com.masai.service;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.masai.exception.EventException;
+import com.masai.exception.UserException;
+import com.masai.model.Customer;
+import com.masai.model.Event;
+import com.masai.repository.UserRepo;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(OrderAnnotation.class)
 public class CustomerServiceTest {
+	
+	@Mock
+	private UserRepo userRepo;
+	
+	@Mock
+	private SecurityContext securityContext;
+	
+	@Mock
+	private Authentication authentication;
+	
+	@InjectMocks
+	private CustomerServiceImpl customerService;
+	
+	private Optional<Customer> opt;
+	
+	@Test
+	@Order(1)
+	public void testRegisterUser() throws UserException {
+		
+		List<Event> al = new ArrayList<>();
+		opt = Optional.of((Customer) new Customer(1, "fn", "ln", "9999999999", "fn@gmail.com", "pass", LocalDate.parse("01-01-2001", DateTimeFormatter.ofPattern("dd-MM-yyy")), al));
+		
+		when(userRepo.findByPhone(anyString())).thenReturn(opt);
+		
+		assertAll(() -> {
+			assertThrows(UserException.class, ()-> customerService.registerUser(opt.get()));
+		});
+		
+		verify(userRepo, never()).save(any());
+		
+	}
+	
+	@Test
+	@Order(2)
+	public void testViewEvents() throws UserException {
+		
+		List<Event> al = new ArrayList<>();
+		opt = Optional.of((Customer) new Customer(1, "fn", "ln", "9999999999", "fn@gmail.com", "pass", LocalDate.parse("01-01-2001", DateTimeFormatter.ofPattern("dd-MM-yyy")), al));
+		
+		when(userRepo.findByPhone(anyString())).thenReturn(opt);
+		
+		when(securityContext.getAuthentication()).thenReturn(authentication);
+		SecurityContextHolder.setContext(securityContext);
+		when(SecurityContextHolder.getContext().getAuthentication().getName()).thenReturn("9999999999");
+		
+		assertAll(() -> {
+			assertThrows(EventException.class, ()-> customerService.viewEvents("month"));
+		});
+		
+	}
+	
+	@Test
+	@Order(3)
+	public void testUpdateUser() throws UserException {
+		
+		List<Event> al = new ArrayList<>();
+		opt = Optional.of((Customer) new Customer(1, "fn", "ln", "9999999999", "fn@gmail.com", "pass", LocalDate.parse("01-01-2001", DateTimeFormatter.ofPattern("dd-MM-yyy")), al));
+		
+		when(userRepo.findByPhone(anyString())).thenReturn(opt);
+		when(userRepo.save(any())).thenReturn(opt.get());
+		
+		when(securityContext.getAuthentication()).thenReturn(authentication);
+		SecurityContextHolder.setContext(securityContext);
+		when(SecurityContextHolder.getContext().getAuthentication().getName()).thenReturn("9999999999");
+		
+		Customer customer = customerService.updateUser(opt.get());
+		
+		assertAll(() -> {
+			assertEquals(1, customer.getUserId());
+		});
+		
+	}
 	
 }
